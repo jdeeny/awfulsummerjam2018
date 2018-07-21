10093fc7d3e623e19af5cd1991bf8e7d3db9bec0
jdeeny <jdeeny@gmail.com>
2018-07-21 15:06:47 -0600

    Add IBM font

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ local datamuse_cache = lru.new(1000)
 function love.load()
   teststr = words.test()
   datamuse_cache = lru.new(1000)
+  ibmfont = love.graphics.newFont('assets/fonts/PxPlus_IBM_VGA8.ttf', 16)
+  love.graphics.setFont(ibmfont)
 end
 
 function love.update(dt)
@@ -14,6 +16,7 @@ end
 
 function love.draw()
   love.graphics.print(teststr ,0, 0)
+  love.graphics.print("╫╓er", 8, 16*10)
 end
 
 function new_game()
