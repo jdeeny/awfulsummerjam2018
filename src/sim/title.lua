84f2763d9433038f02b2c891d67f55d70f156e44
jdeeny <jdeeny@gmail.com>
2018-07-24 16:55:15 -0600

    Some title formats

--- src/sim/title.lua
+++ src/sim/title.lua
@@ -7,3 +7,36 @@ function Title:emit()
   return "A title"
 end
 return Title
+
+
+--[[
+verb-ing noun
+verbing location
+name the noun-phrase or the noun-phrase
+verb they verb a noun and noun verb?
+noun noun noun
+The adjective noun
+word Part 2
+word Part 3
+Why Verb? <noun-phrase list>   (x, y, z)
+preposition/adverb the noun
+name
+first name
+last name
+noun
+noun phrase
+anyword
+adj noun, adj noun
+noun phrase, noun phrase
+A noun for the noun
+Group of capital letters
+A noun of noun
+verb me noun
+The <last = what kind of word?> of the <plurual noun>
+<phrase> - <name>
+<name> - <phrase>
+The adj noun
+<numbered noun> to <noun>
+
+
+]]
