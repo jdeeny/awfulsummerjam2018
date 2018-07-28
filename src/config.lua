aeae49e0acbebdb48b762f77b3bc6527766a07c3
jdeeny <jdeeny@gmail.com>
2018-07-28 12:54:48 -0600

    Calculate game time data

--- src/config.lua
+++ src/config.lua
@@ -6,6 +6,11 @@ local Config = {
   Directors = 15,   -- If this is a history, needs to be per year or something
   Actors = 30,
   Movies = 30,
+  GameInMinutes = 8.0,
+  GameYears = 2.0,
+  DaysPerMonth = 10,
+  MonthsPerYear = 12,
+  MoviesPerYear = 8,
   ThemeNouns = 10,
   ThemeVerbs = 10,
   ThemeAdjectives = 10,
@@ -15,4 +20,12 @@ local Config = {
   MusicFadeTime = 5,
   Controls = require('src/controls'),
 }
+
+Config.GameMonths = Config.GameYears * Config.MonthsPerYear
+Config.GameDays = Config.GameMonths * Config.DaysPerMonth
+Config.GameInSeconds = Config.GameInMinutes * 60.0
+Config.DaysPerSecond = Config.GameDays / Config.GameInSeconds
+
+print("Game is " .. Config.GameDays .." days long, " .. Config.DaysPerSecond.. " days/s = " .. Config.GameInSeconds)
+
 return Config
