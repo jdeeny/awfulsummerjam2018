651e149386d8505e387074fdeb24a50fe33b42eb
jdeeny <jdeeny@gmail.com>
2018-07-30 17:53:28 -0600

    improve intro layout but button doesnt work

--- src/widgets/intropage.lua
+++ src/widgets/intropage.lua
@@ -1,7 +1,7 @@
 local IntroPage = class('IntroPage', Widget)
 
 local row1 = 4
-local row2 = 14
+local row2 = 18
 local col1 = 2
 local colw = (Config.CharWidth - 4) / 2
 local col2 = col1 + colw + 3
@@ -17,15 +17,29 @@ You have been given 2 years to make a profit. It you do good you win.
 function IntroPage:initialize(x,y,w,h)
   Widget.initialize(self,x,y,w,h)
 
-  self.rankings_panel = Widgets.Panel:new(col1,row1,colw,20, "BOX OFFICE")
+  self.rankings_panel = Widgets.Panel:new(col1,row1,colw,16, "BOX OFFICE")
   self.bo_panel = Widgets.BoxOfficePanel:new(col1+1, row1+4, colw-4, 16)
   self.rankings_panel:add(self.bo_panel)
-  self.intro_panel = Widgets.Panel:new(col1,row2,colw, 20, "INTRODUCTION")
-  self.intro_panel:add(Widgets.TextPanel:new(col1+3,row2+3, colw-6, 20,Palette.NormalText, INTRO))
+  self.intro_panel = Widgets.Panel:new(col1,row2,colw, 25, "INTRODUCTION")
+  self.intro_panel:add(Widgets.TextPanel:new(col1+3,row2+3, colw-6, 24,Palette.NormalText, INTRO))
   self.feed_panel = Widgets.Panel:new(col2,row1,colw,self.ch - 5, "NEWS FEED")
   self.feed_panel:add(Widgets.NewsFeed:new(col2+2,row1+3,self.feed_panel.cw-4,self.feed_panel.ch-4))
   --self.finance_panel:add(Widgets.ProjectBars:new(2,15,self.cw-2,15))
 
+  local function go_gameplay ()
+    print('clicked go')
+    if not Game.time.is_historic() then
+      Game.state:jump('Gameplay')
+      Game.state:call('Debounce')
+    end
+  end
+
+  self.continue_button = Widgets.Button:new(10,row2+15,colw-10,7,"Generating History...",
+    {
+      onclick=go_gameplay,
+    } )
+
+  self.intro_panel:add(self.continue_button)
   self:add(self.rankings_panel)
   self:add(self.intro_panel)
   self:add(self.feed_panel)
