3a3eb7e5464fb0aa9648cf6859ecfc7eacbed58d
jdeeny <jdeeny@gmail.com>
2018-07-31 19:01:15 -0600

    now with clicky

--- src/widgets/list.lua
+++ src/widgets/list.lua
@@ -5,6 +5,7 @@ function List:initialize(x, y, w, h)
   self.items = {}
   self.item_height = 4
   self.current_top = 1
+  self.highlighted = 0
 
 end
 
@@ -35,9 +36,37 @@ function List:_draw()
   love.graphics.clear(Palette.Background)
   for i=self.current_top,self.current_top + math.ceil(self.ch / self.item_height) do
     if self.items[i] then
+      love.graphics.setColor(i == self.highlighted and Palette.HighlightText or Palette.NormalText)
       self.items[i]:list_print(self.cx, self.cy + (i-1-self.current_top) * self.item_height, self.cw)
     end
   end
+end
+function List:click(x,y)
+  if self._onclick then self:_onclick(x,y) end
+end
+
+function List:_onclick(x, y)
+  print("click in List")
+  local x, y = x - self.px, y - self.py
+  if x >= 0 and y >= 0 and x <= self.pw and y <= self.ph then
+    local h = math.floor((y-16) /16/ self.item_height)
+    if h < self.ch and self.items[self.current_top+h] then
+      self.highlighted = self.current_top+h
+      self.clean = false
+      self:_show_details(self.current_top+h)
+    end
+    --[[if type(self.options.onclick) == 'function' then
+      print("click")
+      self.options.onclick()
+    end]]
+  else
+    --print("Click outside List: " .. self.text .." ".. x.. " " .. y)
+  end
+  self.mode = 'click'
+end
+
+function List:_show_details(n)
+print ("Show deets")
 end
 
 return List
