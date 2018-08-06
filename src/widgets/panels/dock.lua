e4b0eae61d75597cd87d06ddee13222a475980e1
jdeeny <jdeeny@gmail.com>
2018-08-06 03:38:19 -0600

    some notes in panels

--- src/widgets/panels/dock.lua
+++ src/widgets/panels/dock.lua
@@ -1,3 +1,9 @@
+--[[
+'dock' contents to the left/right/top/bottom of the panel
+
+each item has a dock value that says which side it is added to
+]]
+
 local Panel = require('src.widgets.base.panel')
 
 local Dock = class('Dock', Panel)
--- src/widgets/panels/stack.lua
+++ src/widgets/panels/stack.lua
@@ -1,9 +1,62 @@
+--[[
+Stack the items horizontally or vertically. Items are always
+full size and scrolled if needed.
+]]
 local Panel = require('src.widgets.base.panel')
 
 local Stack = class('Stack', Panel)
 
-function Stack:initialize()
-  Panel.initialize(self)
+local Defaults = {
+  orientation = 'horiz',
+}
+
+function Stack:initialize(options)
+  Panel.initialize(self, Defaults)
+  for k,v in pairs(options or {}) do
+    self.options[k] = v
+  end
+end
+
+function Stack:add(item, n)
+  if n then
+    table.insert(self.contents, n, item)
+  else
+    table.insert(self.contents, item)
+  end
+end
+
+function Stack:_measure(available_w, available_h)
+  if self.options.orientation and self.options.orientation == 'vert' then
+    local contents_w = self._find_widest()
+    local contents_h = self._sum_heights()
+  else
+    local contents_w = self._sum_widths()
+    local contents_h = self._find_tallest()
+  end
+
+  local w = self.options.padding.left + self.options.padding.right +
+            self.options.margin.left + self.options.padding.right +
+            contents_w
+
+  local h = self.options.padding.top + self.options.padding.bottom +
+            self.options.margin.top + self.options.margin.bottom +
+            contents_h
+
+  -- add up all children
+
+  self.desired_size = { w, h }
+  return { w, h }
+end
+
+function Panel:_arrange(allowed_w, allowed_h)
+  if self.options.orientation and self.options.orientation == 'vert' then
+    local contents_w = self._find_widest()
+    local contents_h = self._sum_heights()
+  else
+    local contents_w = self._sum_widths()
+    local contents_h = self._find_tallest()
+  end
+
 end
 
 return Stack
